72650b22521c58f4a8c0b4ca144b0bb1652913eb
gr8fulnez <oluwolejohn72@gmail.com>
2025-06-04 20:07:10 +0100

    first commit

---
+++ HireStream/src/main/java/com/example/HireStream/HireStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.HireStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireStreamApplication.class, args);
+	}
+
+}
---
+++ HireStream/src/test/java/com/example/HireStream/HireStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HireStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
